caf457cca29517032dfd5c051b1cd6641ec91d5d
zhoushengke123 <zhoushengke123@gmail.com>
2017-10-29 02:08:53 -0500

    3

--- static/main3.js
+++ static/main3.js
@@ -176,3 +176,52 @@ function hideUI(){
   error_message.classList.remove("visible");
 }
 
+
+
+
+
+
+
+
+// Check scroll position and add/remove background to navbar
+function checkScrollPosition() {
+    if($(window).scrollTop() > 50) {
+      $(".fixed-header").addClass("scroll");
+  } else {        
+      $(".fixed-header").removeClass("scroll");
+  }
+}
+
+$(document).ready(function () {   
+    // Single page nav
+    $('.fixed-header').singlePageNav({
+        offset: 59,
+        filter: ':not(.external)',
+        updateHash: true        
+    });
+
+    checkScrollPosition();
+
+    // nav bar
+    $('.navbar-toggle').click(function(){
+        $('.main-menu').toggleClass('show');
+    });
+
+    $('.main-menu a').click(function(){
+        $('.main-menu').removeClass('show');
+    });
+});
+
+$(window).on("scroll", function() {
+    checkScrollPosition();    
+});
+
+
+
+
+  // $(document).on('scroll', function() {
+  //   if($(this).scrollTop()>=$('#theTarget').position().top){
+  //     console.log('');
+  //   }
+  // })
+
